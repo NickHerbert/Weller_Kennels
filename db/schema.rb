2af7dc758a08b2923f2bb4b055791954d9f15226
NickHerbert <nherbe3@gmail.com>
2017-10-11 22:08:27 -0400

    removing messages and adding pages. Going to structure like TI

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,5 @@
 
 ActiveRecord::Schema.define(version: 20171011031251) do
 
-  create_table "messages", force: :cascade do |t|
-    t.string   "subject"
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
